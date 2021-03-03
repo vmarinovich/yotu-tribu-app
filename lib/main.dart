a7bc54720427582f1928a1e17da268aa685faaa0
vmarinovich <72771498+vmarinovich@users.noreply.github.com>
2021-03-03 01:22:22 -0500

    Cambiar list view por gridview

--- lib/main.dart
+++ lib/main.dart
@@ -45,10 +45,16 @@ class HomePage extends StatelessWidget {
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (snapshot.hasData) {
                 var posts = snapshot.data;
-                return ListView.separated(
-                    separatorBuilder: (context, index) => Divider(),
+                return GridView.builder(
+                    padding: EdgeInsets.all(0),
                     itemCount: posts.length,
-                    itemBuilder: (context, index) {
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 50.0,
+                      crossAxisSpacing: 10.0,
+                      childAspectRatio: 1.0,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
                       Map post = snapshot.data[index];
 
                       return GestureDetector(
@@ -59,15 +65,19 @@ class HomePage extends StatelessWidget {
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: <Widget>[
                                 FadeInImage(
-                                    placeholder:
-                                        AssetImage('assets/loading.gif'),
+                                    width: 70.0,
+                                    height: 70.0,
+                                    placeholder: AssetImage(
+                                        'assets/imagenes/loading.gif'),
                                     image: NetworkImage(post['acf']['imagen']
                                         ['sizes']['thumbnail'])),
                                 Text(post['name'],
                                     style: TextStyle(
-                                        fontSize: 25.0,
+                                        fontSize: 20.0,
                                         fontWeight: FontWeight.bold)),
-                                Text(post['acf']['fechas'])
+                                Text(post['acf']['fechas'],
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 12.0))
                               ]));
                     });
               } else {
